0169a9a9857f6dda3b57b4f0d6fa21b59a7703d0
ryanfitzer <ryan@ryanfitzer.com>
2012-03-11 14:15:33 -0700

    Using `mainConfigFile` property to consolidate `paths`. Updated RequireJS to 1.0.7

--- library/js/require-build.js
+++ library/js/require-build.js
@@ -2,9 +2,9 @@
     appDir: '../../',
     baseUrl: 'library/js/',
     dir: '../../../requirejs-jquery-build',
-    paths: {
-        jquery: 'modules/jquery'
-    },
+    optimize: 'none',
+    mainConfigFile: 'require-config.js',
+    jQuery: null, //override require-config value https://github.com/jrburke/requirejs/issues/212
     modules: [
         {
             name: 'boot-index',
